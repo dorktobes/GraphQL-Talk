a6b36bd9aeeba7bc3167d4ca12a632bfab6561c4
Toby Hoshman <t.hoshman@gmail.com>
2018-06-18 16:52:23 -0700

    remove Message Type

--- server/schema/types/MessageType.js
+++ server/schema/types/MessageType.js
@@ -8,31 +8,6 @@ const {
   GraphQLID,
 } = graphql;
 
-const MessageType = new GraphQLObjectType({
-  name: 'MessageType',
-  fields: () => ({
-    id: {
-      type: GraphQLID,
-      resolve(parentValue) {
-        return parentValue._id;
-      },
-    },
-    text: {
-      type: GraphQLString,
-    },
-    user: {
-      type: UserType,
-      resolve({ user }, args, context) {
-        return context.models.Users.getByID(user);
-      },
-    },
-    room: {
-      type: RoomType,
-      resolve({ room }, args, context) {
-        return context.models.Rooms.getById(room);
-      },
-    },
-  }),
-});
+const MessageType = {};
 
 module.exports = MessageType;
